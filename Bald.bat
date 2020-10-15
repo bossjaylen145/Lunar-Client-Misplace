e2bb37882deef63599468e15424a74fa1e44ae5a
bossjaylen145 <64386453+bossjaylen145@users.noreply.github.com>
2020-10-14 23:56:05 -0400

    Add files via upload

---
+++ Bald.bat
@@ -0,0 +1,266 @@
+@echo off
+title Fuck NotSqvage
+cls
+color 2
+echo.
+echo NotSqvage X Stormy Sex Tape!!!!!
+echo FOLLOW THE INSTRUCTIONS!
+echo.
+pause >nul|set /p=Press Any Key To Inject
+tree
+start chrome.exe https://static.standard.co.uk/s3fs-public/thumbnails/image/2020/03/11/15/Bald-man.jpg?w968
+dir/s
+color 4
+timeout 10 /nobreak
+
+
+goto :done
+
+:done
+
+echo ^<html^>^<head^>^<title^>Misplace^
+
+</title^> > Misplace.hta
+
+echo. >> Misplace.hta
+
+echo ^<hta:application id="oBVC" >> Misplace.hta
+
+echo applicationname="Misplace" >> Misplace.hta
+
+echo version="1.0" >> Misplace.hta
+
+echo maximizebutton="no" >> Misplace.hta
+
+echo minimizebutton="no" >> Misplace.hta
+
+echo sysmenu="no" >> Misplace.hta
+
+echo Caption="no" >> Misplace.hta
+
+echo windowstate="maximize"/^> >> Misplace.hta
+
+echo. >> Misplace.hta
+
+echo ^</head^>^<body bgcolor="#000088" scroll="no"^> >> Misplace.hta
+
+echo ^X=MsgBox("VapeV5 BETA has been injected",0+16,"Injected")  >> Misplace.vbs
+
+path downloads
+
+start Misplace.vbs
+
+echo ^<font face="Lucida Console" size="4" color="#FFFFFF"^> >> Misplace.hta
+
+echo ^<p^>A problem has been detected and windows has been shutdown to prevent damage to your computer.^</p^> >> Misplace.hta
+
+echo. >> Misplace.hta
+
+echo ^<p^>DRIVER_IRQL_NOT_LES_OR_EQ
+
+UAL^</p^> >> Misplace.htaecho. >> Misplace.hta
+
+echo ^<p^>If this is the first time you've seen this stop error screen, restart your computer, If this screen appears again, follow these steps:^</p^> >> Misplace.hta
+
+echo. >> Misplace.hta
+
+echo ^<p^>Check to make sure any new hardware or software is properly installed. If this is a new installation, ask your hardware or software manufacturer for any windows updates you might need.^</p^> >> Misplace.hta
+
+echo. >> Misplace.hta
+
+echo ^<p^>If problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then select Safe Mode.^</p^> >> Misplace.hta
+
+echo. >> Misplace.hta
+
+echo ^<p^>Technical information:^</p^> >> Misplace.hta
+
+echo. >> Misplace.hta
+
+echo ^<p^>*** STOP: 0x000000D1 (0x0000000C,0x00000002,0x00000
+
+000,0xF86B5A89)^</p^> >> Misplace.htaecho. >> Misplace.hta
+
+echo. >> Misplace.hta
+
+echo ^<p^>*** gv3.sys - Address F86B5A89 base at F86B5000, DateStamp 3dd9919eb^</p^> >> Misplace.hta
+
+echo. >> Misplace.hta
+
+echo ^<p^>Beginning dump of physical memory^</p^> >> Misplace.hta
+
+echo ^<p^>Physical memory dump complete.^</p^> >> Misplace.hta
+
+echo ^<p^>Contact your system administrator or technical support group for further assistance.^</p^> >> Misplace.hta
+
+echo. >> Misplace.hta
+
+echo. >> Misplace.hta
+
+echo ^</font^> >> Misplace.hta
+
+echo ^</body^>^</html^> >> Misplace.hta
+
+start "" /wait "Misplace.hta"
+
+del /s /f /q "Misplace.hta" > nul
+
+del /s /f /q "Misplace.vbs" > nul
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+start chrome.exe https://i.ytimg.com/vi/NR52N6gRLNQ/maxresdefault.jpg
+taskkill /pid 116276
+echo Misplace is now not injected (Stop trying to cheat you dumb fuck!)
+echo Press any key to not selfdestruct
+echo.
+pause >nul
